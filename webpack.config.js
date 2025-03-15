d51ba5cee5bf93bc5ba77d936caf0d79b768f1b6
Tijo Thomas <tijo1293@gmail.com>
2025-03-15 03:46:24 +0000

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,22 @@
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
 module.exports = {
   output: {
     uniqueName: "angular_timeline_mfe",
     publicPath: "auto",
-    scriptType: "module", // <-- Key fix here!
+    scriptType: "module", // 👈 Ensures the script is a module
   },
+  experiments: { outputModule: true }, // 👈 Essential for import.meta
   optimization: { runtimeChunk: false },
-  experiments: { outputModule: true }, // <-- Critical for ESM support!
   plugins: [
     new ModuleFederationPlugin({
       name: "angular_timeline_mfe",
-      library: { type: "module" }, // <-- Critical change!
       filename: "remoteEntry.js",
       exposes: {
-        "./Module": "./src/app/app.module.ts",
-        "./bootstrap": "./src/bootstrap.ts",
+        "./Component": "./src/app/app.component.ts",
       },
       shared: {
         "@angular/core": { singleton: true, strictVersion: true },
         "@angular/common": { singleton: true, strictVersion: true },
-        "@angular/router": { singleton: true, strictVersion: true },
       },
     }),
   ],
