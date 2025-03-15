86d03bc51c198e0c9ccde34132c20cc96b8d8c76
Tijo Thomas <tijo1293@gmail.com>
2025-03-15 02:29:48 +0000

    webpack module fedaration update

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  output: {
+    uniqueName: "angular_timeline_mfe",
+    publicPath: "auto",
+  },
+  optimization: { runtimeChunk: false },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "angular_timeline_mfe",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Component": "./src/app/app.component.ts",
+      },
+      shared: {
+        "@angular/core": { singleton: true, strictVersion: true },
+        "@angular/common": { singleton: true, strictVersion: true },
+        "@angular/router": { singleton: true, strictVersion: true },
+      },
+    }),
+  ],
+};
